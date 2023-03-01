02f6b8c5e66101e53f197e2d5ac861e757f956f7
Jaroslav Lhotak <lhotakj@gmail.com>
2023-03-01 23:24:09 +0100

    join

--- init/config_factory.py
+++ init/config_factory.py
@@ -10,22 +10,22 @@ import click
 @click.argument('template', type=click.Path(exists=True))
 @click.argument('output', type=click.Path(exists=False))
 def config_factory(template, output):
-    print("Reading template ${template} ...")
+    print(f"Reading template {template} ...")
     read_file: IO = open(template, mode='r')
     content: str = read_file.read()
     read_file.close()
 
     for name, value in os.environ.items():
         if name.startswith('TF_VAR_'):
+            print(f" - {name}")
             content = content.replace('${' + name + '}', value)
-            content = content.replace('$' + name + '', value)
 
-    print("Writing output file ${output} ...")
+    print(f"Writing output file {output} ...")
     write_file: IO = open(output, "w")
     write_file.write(content)
     write_file.close()
 
 
 if __name__ == '__main__':
-    print("Config factory 0.1")
+    print(f"Config factory 0.1")
     config_factory()
